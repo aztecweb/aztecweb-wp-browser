f0382a68ad17acab946de3f8d947ab597e88fd5b
fafera <fafa.capellari@gmail.com>
2026-09-16 23:10:06 -0300

    Add Legacy-side default meta table assertion for subscriptions
    Mirrors SubscriptionHPOSCest's testSubscriptionDefaultMetaTargetsWcOrdersMetaTable so both storages have symmetric coverage for the default keys seeded by haveSubscriptionInDatabase.

--- tests/acceptance/SubscriptionCest.php
+++ tests/acceptance/SubscriptionCest.php
@@ -374,4 +374,29 @@ class SubscriptionCest
             'meta_value' => '0',
         ]);
     }
+
+    public function testSubscriptionDefaultMetaTargetsPostmetaTable(AcceptanceTester $I): void
+    {
+        $subscriptionId = $I->haveSubscriptionInDatabase();
+
+        $defaultKeys = [
+            '_billing_period',
+            '_billing_interval',
+            '_subscription_start_date',
+            '_subscription_expiry_date',
+            '_subscription_end_date',
+        ];
+
+        foreach ($defaultKeys as $metaKey) {
+            $I->seeInDatabase('wp_postmeta', [
+                'post_id' => $subscriptionId,
+                'meta_key' => $metaKey,
+            ]);
+
+            $I->dontSeeInDatabase('wp_wc_orders_meta', [
+                'order_id' => $subscriptionId,
+                'meta_key' => $metaKey,
+            ]);
+        }
+    }
 }
